a306b5a19bf935857d51db14b24e731efb9cb712
JZorvan <jzorvan@gmail.com>
2017-10-19 14:55:27 -0500

    Methods for: collecting xls files, csv file, and converting xls to csv

--- UploadConverter/Program.cs
+++ UploadConverter/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Office.Interop.Excel;
 
 namespace UploadConverter
 {
@@ -10,6 +12,41 @@ namespace UploadConverter
     {
         static void Main(string[] args)
         {
+            string filename = "";
+            char[] fileExt = { '.', 'x', 'l', 's' };
+            Application excel = new Application();
+            Workbook workbook = null;
+            excel.DisplayAlerts = false;
+
+            convertToCSV(readXLSFiles());
+
+            excel.DisplayAlerts = true;
+            excel.Quit();
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(excel);
+            Console.ReadKey();
+
+            string[] readXLSFiles()
+            {
+                string[] XLSFiles = Directory.GetFiles("C:\\Users\\AG20375\\Documents\\Uploads", "*.xls*");
+                return XLSFiles;
+            }
+
+            string[] readCSVFiles()
+            {
+                string[] CSVFiles = Directory.GetFiles("C:\\Users\\AG20375\\Documents\\Uploads", "*.csv");
+                return CSVFiles;
+            }
+            void convertToCSV(string[] XLSFiles)
+            {
+                foreach (string file in XLSFiles)
+                {
+                    filename = file.Trim(fileExt);
+                    workbook = excel.Workbooks.Open(file, ReadOnly: false, Editable: true);
+                    workbook.SaveAs(Filename: filename + ".csv", FileFormat: XlFileFormat.xlCSV);
+                    workbook.Close();
+                }
+            }
         }
     }
 }
